b9f94c5c76c4f669acc6701905e87cf477e84e06
Roy Lau <64418004+roylau98@users.noreply.github.com>
2021-02-05 19:02:28 +0800

    Update map_edits.py

--- map_edits.py
+++ map_edits.py
@@ -1,18 +1,28 @@
 from datetime import datetime
 
+
 class Map:
-    def __init__(self, targets, airplane=[]):
+    """
+    Map Class
+    Attributes: Number of targets (tracks number of targets),
+                List of Airplanes (tracks airplanes in the map),
+                List of Traps (tracks traps in the map)
+    Methods: lap
+    """
+    def __init__(self, targets: int, airplane=[], traps=[]):
         self.targets = targets
         self.airplane = airplane
+        self.traps = traps
         self.time = datetime.now
         self.leaderboard = []
 
     def lap(self):
+        """
+        Calculates the time taken for each airplane to go through all hoops. Called if any airplane passed all targets
+        :return: updates the leaderboard attribute with a tuple (minutes, seconds) based on the time taken
+        """
         curr = datetime.now  # get the current time
         diff = (self.time - curr).total_seconds()
         minutes = diff / 60  # get minutes passed
         seconds = round(diff - (minutes * 60))
         self.leaderboard.append((minutes, seconds))
-
-    def checkTargets(self):
-        #check if location of plane == location of targets. if so then add points for airplane
